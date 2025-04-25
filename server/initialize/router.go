5a44e42db85f44821700bacca802dc209a44e6a8
RZXBXie <2545147971@qq.com>
2025-04-25 17:32:21 +0800

    feat:新增looger和recovery中间件

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"server/global"
+	"server/middleware"
 	"server/router"
 )
 
@@ -14,16 +15,17 @@ func InitRouter() *gin.Engine {
 	// 设置gin模式
 	gin.SetMode(global.Config.System.Env)
 	Router := gin.Default()
-
+	Router.Use(middleware.GinLogger(), middleware.GinRecovery(true))
+	
 	// 使用gin会话路由
 	var store = cookie.NewStore([]byte(global.Config.System.SessionsSecret))
 	Router.Use(sessions.Sessions("session", store))
-
+	
 	// 将指定目录下的文件提供给客户端
 	// "uploads" 是URL路径前缀，http.Dir("uploads")是实际文件系统中存储文件的目录
 	Router.StaticFS(global.Config.Upload.Path, http.Dir(global.Config.Upload.Path))
 	routerGroup := router.RouterGroupApp
-
+	
 	// 公共路由，api开头
 	publicGroup := Router.Group(global.Config.System.RouterPrefix)
 	{
